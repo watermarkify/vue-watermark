539f7ceeda8c6bfa538d30e79e91d384112d9f79
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-05 18:33:09 +0800

    feat: add playground

---
+++ playground/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import Watermark from '@watermarkify/vue-watermark'
+
+const watermarkOptions = ref({
+    content: 'Confidential',
+  },
+)
+</script>
+
+<template>
+  <div>
+    <h1>Watermark Example</h1>
+    <Watermark :options="watermarkOptions">
+      <p>This is the content of the slot.</p>
+    </Watermark>
+  </div>
+</template>

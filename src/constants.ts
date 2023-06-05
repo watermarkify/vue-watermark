7484fabaf666f24117b13447b27ce0a7884b4958
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-05 18:32:17 +0800

    feat: add InjectionOptions

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+import type { InjectionKey } from 'vue'
+import type { WatermarkOptions } from './types'
+
+export const InjectionOptions = 'vue-watermark-options' as unknown as InjectionKey<WatermarkOptions>

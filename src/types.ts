e37077bcbc4fdb0cb8c1920c59a58989b80b66c0
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-06 00:39:55 +0800

    feat: add WatermarkFont and revise comments

--- src/types.ts
+++ src/types.ts
@@ -1,42 +1,18 @@
+export interface WatermarkFont {
+  color?: string // Color of the watermark text
+  fontSize?: number | string // Font size of the watermark text
+  fontWeight?: 'normal' | 'light' | 'weight' | number // Font weight of the watermark text
+  fontStyle?: 'none' | 'normal' | 'italic' | 'oblique' // Font style of the watermark text
+  fontFamily?: string // Font family of the watermark text
+}
+
 export interface WatermarkOptions {
-  /**
-     * The width of the watermark in pixels.
-     */
-  width?: number
-
-  /**
-     * The height of the watermark in pixels.
-     */
-  height?: number
-
-  /**
-     * The URL or path to the image file to be used as the watermark.
-     */
-  image?: string
-
-  /**
-     * The text content to be used as the watermark.
-     * It can be a single string or an array of strings to display multiple lines of text.
-     */
-  content?: string | string[]
-
-  /**
-     * The gap between each instance of the watermark in pixels.
-     * It is specified as a tuple where the first element represents the horizontal gap and
-     * the second element represents the vertical gap.
-     */
-  gap?: [number, number]
-
-  /**
-     * The offset of the watermark from the top-left corner of the container element.
-     * It is specified as a tuple where the first element represents the horizontal offset and
-     * the second element represents the vertical offset.
-     */
-  offset?: [number, number]
-
-  /**
-     * The z-index of the watermark relative to other elements on the page.
-     * A higher value means the watermark will appear on top of other elements with lower z-index values.
-     */
-  zIndex?: number
+  width?: number // The width of the watermark in pixels.
+  height?: number // The height of the watermark in pixels.
+  image?: string // The URL or path to the image file to be used as the watermark.
+  content?: string | string[] // The text content to be used as the watermark.
+  gap?: [number, number] // The gap between each instance of the watermark in pixels.
+  offset?: [number, number] // The offset of the watermark from the top-left corner of the container element.
+  zIndex?: number // The z-index of the watermark relative to other elements on the page.
+  font?: WatermarkFont // The font properties for the watermark text.
 }

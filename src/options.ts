020ab0c96628d5304fd7927a7b708e2456cfd945
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-05 23:19:57 +0800

    feat: add defaultOptions

---
+++ src/options.ts
@@ -0,0 +1,16 @@
+export const defaultOptions = {
+  width: 120,
+  height: 50,
+  content: 'watermark',
+  image: null,
+  gap: [20, 10],
+  offset: [5, 5],
+  zIndex: 5,
+  font: {
+    color: 'rgba(0,0,0,.2)',
+    fontSize: 18,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontFamily: 'sans-serif',
+  }
+}

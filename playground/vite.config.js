3b811cef4e912c6362b036e3e605c9f2a4a0b771
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-05 18:32:42 +0800

    feat: add vite.config.js

---
+++ playground/vite.config.js
@@ -0,0 +1,14 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+      '@watermarkify/vue-watermark': path.resolve(__dirname, '../src/index.ts'),
+    },
+  },
+  plugins: [vue()],
+})

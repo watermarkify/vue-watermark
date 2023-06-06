24645a72df76303586e528257036eace93bf4708
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-06 15:36:52 +0800

    feat: add FontGap and BaseSize

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,5 @@ import type { InjectionKey } from 'vue'
 import type { WatermarkOptions } from './types'
 
 export const InjectionOptions = 'vue-watermark-options' as unknown as InjectionKey<WatermarkOptions>
+export const FontGap = 3
+export const BaseSize = 2

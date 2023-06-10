31ceaed52da8dbd0ee3829353661727f4fbbca82
Wing-Kam Wong <wingkwong.code@gmail.com>
2023-06-10 14:30:55 +0800

    feat(playground): import windicss

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import Watermark from '@watermarkify/vue-watermark'
 import './style.css'
 import App from './App.vue'
+import 'virtual:windi.css'
 
 const app = createApp(App)
 app.use(Watermark())

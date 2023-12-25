ce8beafa51f50705f59b1c83a6ae7f5ad5509fad
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2023-12-25 14:11:40 +0800

    chore: lint

--- playground/windi.config.ts
+++ playground/windi.config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from 'windicss/helpers'
-import colors from 'windicss/colors'
 import plugin from 'windicss/plugin'
 
 export default defineConfig({
-    theme: {
-        extend: {
-            colors: {
-                watermarkify: {
-                    500: '#62D7F4',
-                },
-            },
+  theme: {
+    extend: {
+      colors: {
+        watermarkify: {
+          500: '#62D7F4',
         },
+      },
     },
-    plugins: [
-        plugin(({ addUtilities }) => {
-          const newUtilities = {
-            '.flex-2': {
-              flex: '2',
-            },
-          }
-          addUtilities(newUtilities)
-        }),
-    ],
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      const newUtilities = {
+        '.flex-2': {
+          flex: '2',
+        },
+      }
+      addUtilities(newUtilities)
+    }),
+  ],
 })
